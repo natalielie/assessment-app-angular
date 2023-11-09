e17b004093f495bfbaa400e903ec066be54ef036
Наталія Малищенко <nataliia.malyshchenko@nure.ua>
2023-11-09 19:59:39 +0200

    add spinner service

--- src/app/interceptors/token.interceptor.ts
+++ src/app/interceptors/token.interceptor.ts
@@ -5,28 +5,33 @@ import {
   HttpEvent,
   HttpInterceptor,
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, finalize } from 'rxjs';
+import { LoaderService } from '../services/loader.service';
 
 /**
  * an interceptor for adding a token to a header for every api request
  */
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
-  constructor() {}
+  constructor(private loaderService: LoaderService) {}
 
   intercept(
     request: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
+    this.loaderService.showLoader();
     if (request.headers.get('skip')) {
-      return next.handle(request);
+      return next
+        .handle(request)
+        .pipe(finalize(() => this.loaderService.hideLoader()));
     }
     request = request.clone({
       setHeaders: {
         'X-Token': localStorage.getItem('token')!,
       },
     });
-
-    return next.handle(request);
+    return next
+      .handle(request)
+      .pipe(finalize(() => this.loaderService.hideLoader()));
   }
 }
