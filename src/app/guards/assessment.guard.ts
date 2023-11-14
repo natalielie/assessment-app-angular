27e2dcf04a34fea5cbe2cfabe16532d9a7ba26e1
Наталія Малищенко <nataliia.malyshchenko@nure.ua>
2023-11-14 14:18:31 +0200

    fix assessment guard

--- src/app/guards/assessment.guard.ts
+++ src/app/guards/assessment.guard.ts
@@ -1,38 +1,31 @@
-import { Injectable, inject } from '@angular/core';
+import { inject } from '@angular/core';
 import {
   ActivatedRouteSnapshot,
-  RouterStateSnapshot,
   Router,
   CanActivateFn,
-  CanActivate,
   UrlTree,
 } from '@angular/router';
 import { dashboardPath } from '../shared/globals';
 import { ApiService } from '../services/api.service';
 import { Observable, map, take, tap } from 'rxjs';
-import { AdminGuard } from './admin.guard';
 import { AuthService } from '../auth/service/auth.service';
 
 export const AssessmentGuard: CanActivateFn = (
   route: ActivatedRouteSnapshot
 ): Observable<boolean | UrlTree> | boolean | UrlTree => {
   const apiService = inject(ApiService);
-  const authService = inject(AuthService);
   const router = inject(Router);
+  let isUsersAssessment!: boolean;
 
-  if (authService.isAdmin()) {
-    return true;
-  }
   return apiService.getAssessments().pipe(
     tap((assessments) =>
-      assessments.forEach((assessment) => {
-        if (assessment.id === +route.params['id']) {
-          return true;
+      assessments.forEach((report) => {
+        if (report.id === +route.params['id']) {
+          isUsersAssessment = true;
         }
-        return router.navigate([dashboardPath]);
       })
     ),
     take(1),
-    map((assessment) => !!assessment)
+    map((assessment) => isUsersAssessment ?? router.navigate([dashboardPath]))
   );
 };
